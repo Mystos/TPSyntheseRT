ab0d12eca956cfb7cb85cbd7b5a1d4ddda06289a
Az <thomas.guyamier+Kraken@gmail.com>
2020-09-15 11:48:32 +0200

    Ajout Projet et Creation image

---
+++ TPSyntheseRT/Program.cs
@@ -0,0 +1,29 @@
+﻿using SFML.Graphics;
+using System;
+
+
+namespace TPSyntheseRT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint width = 100;
+            uint height = 100;
+
+            // Create a 10x10 image filled with black color
+            Image image = new Image(width, height, Color.Red);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+
+                }
+            }
+            image.SaveToFile("result.png");
+
+
+        }
+    }
+}
