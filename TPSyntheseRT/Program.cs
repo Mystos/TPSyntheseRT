b21e3c8fbb9b2c992b1f8970b57b719ce25adfcc
Az <thomas.guyamier+Kraken@gmail.com>
2020-09-21 19:55:09 +0200

    Ajout CalculIntensité

--- TPSyntheseRT/Program.cs
+++ TPSyntheseRT/Program.cs
@@ -1,8 +1,10 @@
 using SFML.Graphics;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 using System.Xml;
+
 
 namespace TPSyntheseRT
 {
@@ -13,8 +15,12 @@ namespace TPSyntheseRT
             uint width = 1000;
             uint height = 1000;
             Image image = new Image(width, height, Color.Cyan);
-            Sphere sphere = new Sphere(new Vector3(width / 2, height / 2, 400), 200);
-            Vector3 L = sphere.Center - Vector3.UnitY * (sphere.Radius + 100);
+            List<Sphere> sphereList = new List<Sphere>();
+            sphereList.Add(new Sphere(new Vector3(0, height / 2, 400), 200));
+            sphereList.Add(new Sphere(new Vector3(width, height / 2, 400), 200));
+            sphereList.Add(new Sphere(new Vector3(width /2, 0, 400), 200));
+            sphereList.Add(new Sphere(new Vector3(width /2, height, 400), 200));
+            Vector3 L = new Vector3(width / 2, height / 2, 300);
             float epsilon = 0.1f;
 
             for (uint y = 0; y < height; y++)
@@ -23,25 +29,34 @@ namespace TPSyntheseRT
                 {
                     Ray ray = new Ray(new Position(new Vector3(x, y, 0)), new Direction(new Vector3(0, 0, 1)));
                     float t;
-                    if (Intersect_Ray_Sphere(ray, sphere, out t))
+
+                    foreach(Sphere sphere in sphereList)
                     {
-                        // On recupere la position de x
-                        Vector3 xPos = ray.StartPosition.Origin + t * ray.Direction.Dir;
-                        // On renvoie un rayon depuis xPos vers L
-                        Vector3 Pep = xPos - epsilon * ray.Direction.Dir;
-                        Ray rayFromX = new Ray(new Position(Pep), new Direction(Vector3.Normalize(L - Pep)));
-                        float tL;
-                        if (Intersect_Ray_Sphere(rayFromX, sphere, out tL))
+                        if (Intersect_Ray_Sphere(ray, sphere, out t))
                         {
-                            //byte color = (byte)(tL / (L - sphere.Center).Length() * 255);  
-                            //image.SetPixel(x, y, new Color(color, color, color));// Lumiere
-                            image.SetPixel(x, y, Color.Black);// Ombre
-                        }
-                        else
-                        {
-                            image.SetPixel(x, y, Color.White); // Lumiere
+                            // On recupere la position de x
+                            Vector3 xPos = ray.StartPosition.Origin + t * ray.Direction.Dir;
+                            // On renvoie un rayon depuis xPos vers L
+                            Vector3 Pep = xPos - epsilon * ray.Direction.Dir;
+                            Ray rayFromX = new Ray(new Position(Pep), new Direction(Vector3.Normalize(L - Pep)));
+                            float tL;
+                            if (Intersect_Ray_Sphere(rayFromX, sphere, out tL))
+                            {
+                                image.SetPixel(x, y, Color.Black);// Ombre
+                            }
+                            else
+                            {
+                                Vector3 N = Vector3.Normalize(Pep - sphere.Center);
+                                Vector3 le = new Vector3(1000, 1000, 1000);
+                                Vector3 A = new Vector3(1, 0, 0);
+                                Vector3 couleurPix = CalculInstensity(1, N, Vector3.Normalize(L - Pep), Pep, L, le, A);
+
+                                Color col = new Color((byte)couleurPix.X, (byte)couleurPix.Y, (byte)couleurPix.Z);
+                                image.SetPixel(x, y, col); // Lumiere
+                            }
                         }
                     }
+                    
                 }
             }
             image.SaveToFile("result.png");
@@ -91,6 +106,22 @@ namespace TPSyntheseRT
             return result;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="V">la visibilité entre x et y (i.e. est-ce que x voit y sans obstacle, i.e. 0 ou 1)</param>
+        /// <param name="N">N c'est la normal à la surface</param>
+        /// <param name="L">L c'est le vecteur en direction de la lampe (i.e. XY normalisé)</param>
+        /// <param name="x">Point x</param>
+        /// <param name="y">Point y</param>
+        /// <param name="LQ">La quantité de lumière émise par la lampe dans la direction du point éclairée</param>
+        /// <param name="A">Albédo, ou couleur de la surface. Pour une surface rouge (1, 0, 0)</param>
+        /// <returns></returns>
+        public static Vector3 CalculInstensity(float V, Vector3 N, Vector3 L, Vector3 x, Vector3 y, Vector3 LQ,Vector3 A)
+        {
+            float cosT = Math.Abs(Vector3.Dot(N, L));
+            float dist2 = Vector3.Distance(x, y) * Vector3.Distance(x, y);
+            return V * cosT * LQ * A / (dist2 * (float)Math.PI);
+        }
     }
 }
