011821a00b8ba12ca01fd93c097e0715e16a9652
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-06 11:20:26 +0200

    Ajout Classe Box

---
+++ TPSyntheseRT/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TPSyntheseRT
+{
+    class Box
+    {
+        Vector3 start;
+        Vector3 end;
+
+        public Box(Vector3 _start, Vector3 _end)
+        {
+            this.start = _start;
+            this.end = _end;
+        }
+
+    }
+}
