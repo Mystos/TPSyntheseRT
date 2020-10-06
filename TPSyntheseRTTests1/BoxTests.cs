f7afbfc71f431665fa7aed32bbcdaab802e34660
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-06 12:04:37 +0200

    Box Test Unitaires

---
+++ TPSyntheseRTTests1/BoxTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TPSyntheseRT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace TPSyntheseRT.Tests
+{
+    public class BoxTests
+    {
+        [TestMethod()]
+        public void UnionBoxTest()
+        {
+            Sphere sphere = new Sphere(new Vector3(0, 0, 0), 1);
+            Sphere sphere2 = new Sphere(new Vector3(0, 0, 0), 2);
+
+            Box box1 = new Box(sphere);
+            Box box2 = new Box(sphere2);
+
+            Box uBox = Box.UnionBox(new Box[] { box1, box2 });
+
+            Assert.AreEqual(box2, uBox);
+
+            Assert.Fail();
+        }
+    }
+}
