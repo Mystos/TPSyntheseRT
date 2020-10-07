718bc6008ce4b77a542fdecce6695b7e7e68c081
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-07 16:13:26 +0200

    Ajout d'un objet Scene contenant tout les objets

---
+++ TPSyntheseRT/Scene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPSyntheseRT
+{
+    public class Scene
+    {
+        public List<Object3D> objectsInScene = new List<Object3D>();
+        public List<Lamp> lamps = new List<Lamp>();
+
+        public Scene()
+        {
+
+        }
+
+    }
+
+
+
+}
