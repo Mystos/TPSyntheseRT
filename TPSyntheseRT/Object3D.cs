9a924836dd5ff188a520341b31337bf292bfeb90
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-07 16:11:12 +0200

    Ajout Object3D
    Classe générique d'objet 3D

---
+++ TPSyntheseRT/Object3D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TPSyntheseRT
+{
+    public abstract class Object3D
+    {
+        public SurfaceType type = SurfaceType.Diffuse;
+        public Vector3 albedo = new Vector3(1, 1, 1);
+        public Vector3 center = Vector3.Zero;
+
+
+    }
+
+    public enum SurfaceType
+    {
+        Reflective,
+        Diffuse,
+        Metalic
+    }
+}
