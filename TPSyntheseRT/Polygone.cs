57ba67d7b2e884c8bac46b8246107994d02f2108
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-07 16:11:31 +0200

    Ajout de la classe polygones pour stocker les polys

---
+++ TPSyntheseRT/Polygone.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics.Glsl;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TPSyntheseRT
+{
+    class Polygone : Object3D
+    {
+        public readonly List<Vector3> listVerticies = new List<Vector3>(); 
+        public readonly List<int> listIndexes = new List<int>();
+
+        
+        public Polygone(List<Vector3> listVerticies, List<int> listIndexes)
+        {
+            this.listVerticies = listVerticies;
+            this.listIndexes = listIndexes;
+            this.center = GetGravityCenter(listVerticies);
+        }
+
+        public static Vector3 GetGravityCenter(List<Vector3> listVertex)
+        {
+            Vector3 cg = Vector3.Zero;
+            foreach (Vector3 vec in listVertex)
+            {
+                cg += vec;
+            }
+            return cg / listVertex.Count;
+        }
+    }
+}
